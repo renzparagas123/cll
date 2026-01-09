1c81eb55d33cb2af8416c019b3801715ce2e7719
jakeu02 <jakeparagas33@gmail.com>
2026-01-09 09:30:28 +0800

    update server

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ const corsOptions = {
     : ['http://localhost:5173', 'http://localhost:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Account-Id', 'X-Lazada-Token']  // ← ADD THIS
 };
 
 // Middleware
